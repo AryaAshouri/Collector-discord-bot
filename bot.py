dbfc523d9a01a42ae6d85081f1683434a445b021
DarkCloud9000 <arya.tesla1856@gmail.com>
2023-06-28 18:42:36 +0330

    init git

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord, colorama
+from colorama import Fore
+from discord.ext import commands
+from art import text2art
+from Data import *
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="")
+
+@bot.event
+async def on_ready():
+    print(text2art("Bot  is  ready"))
+
+@bot.event
+async def on_message(message):
+    send(message)
+    await bot.process_commands(message)
+
+@bot.event
+async def on_message_delete(message):
+    delete(message)
+
+@bot.event
+async def on_message_edit(before, after):
+    edit(before, after)
+
+@bot.command()
+async def clear(message, num):
+    author_all_roles = []
+    for i in message.author.roles:
+        author_all_roles.append(i.name)
+    if ("Administrator" in author_all_roles):
+        await message.channel.purge(limit=int(num)+1)
+    else:
+        await message.reply(f"Hey {message.author} only admins could use this command")
+
+bot.run("MTEyMjkzOTUyNDg1MTQ0OTg5MA.Gea3w2.oiW_i68PvvRPlLqYw-TrESLFBSJpsfRxuUDxX8")
---
+++ data.py
@@ -0,0 +1,101 @@
+import sqlite3, datetime
+from datetime import datetime
+
+def send(message):
+    now = datetime.now()
+    connection = sqlite3.connect("Database/Data.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Send(
+    user_name,
+    user_id,
+    method,
+    content,
+    "date",
+    "time")''')
+
+    cursor.execute('''INSERT INTO Send(
+    user_name,
+    user_id,
+    method,
+    content,
+    "date",
+    "time")
+
+    VALUES (?, ?, ?, ?, ?, ?)''', (
+    message.author.name,
+    message.author.id,
+    "send",
+    message.content,
+    now.strftime("%Y:%m:%d"),
+    now.strftime("%H:%M:%S")))
+
+    connection.commit()
+    connection.close()
+
+def delete(message):
+    now = datetime.now()
+    connection = sqlite3.connect("Database/Data.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS "Delete"(
+    user_name,
+    user_id,
+    method,
+    content,
+    "date",
+    "time")''')
+
+    cursor.execute('''INSERT INTO "Delete"(
+    user_name,
+    user_id,
+    method,
+    content,
+    "date",
+    "time")
+
+    VALUES (?, ?, ?, ?, ?, ?)''', (
+    message.author.name,
+    message.author.id,
+    "delete",
+    message.content,
+    now.strftime("%Y:%m:%d"),
+    now.strftime("%H:%M:%S")))
+
+    connection.commit()
+    connection.close()
+
+def edit(before, after):
+    now = datetime.now()
+    connection = sqlite3.connect("Database/Data.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Edit(
+    user_name,
+    user_id,
+    method,
+    before,
+    after,
+    "date",
+    "time")''')
+
+    cursor.execute('''INSERT INTO Edit(
+    user_name,
+    user_id,
+    method,
+    before,
+    after,
+    "date",
+    "time")
+
+    VALUES (?, ?, ?, ?, ?, ?, ?)''', (
+    after.author.name,
+    after.author.id,
+    "edit",
+    before.content,
+    after.content,
+    now.strftime("%Y:%m:%d"),
+    now.strftime("%H:%M:%S")))
+
+    connection.commit()
+    connection.close()
